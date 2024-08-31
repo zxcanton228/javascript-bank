40e97534868e7819245fad957593134697e96b91
Кирилл Вегеле <kirillvegele10@gmail.com>
2024-08-31 15:31:28 +0300

    Ready RenderService and FTN

--- src/components/screens/home/home.components.js
+++ src/components/screens/home/home.components.js
@@ -1,10 +1,15 @@
 import BaseScreen from '@/core/components/base-screen.components'
+import renderService from '@/core/services/render.service'
+
+import styles from './home.module.scss'
+import template from './home.template.html'
 
 export class Home extends BaseScreen {
 	constructor() {
 		super({ title: 'Home' })
 	}
 	render() {
-		return '<p>Home<p/>'
+		const element = renderService.htmlToElement(template, [], styles)
+		return element.outerHTML
 	}
 }
